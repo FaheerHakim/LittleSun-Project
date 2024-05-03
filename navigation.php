acac28b349c7b5c03a1476b6ce2d843aee39f791
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-05-03 23:42:55 +0200

    juiste href

--- navigation.php
+++ navigation.php
@@ -17,12 +17,10 @@
     <div class="sidebar">
         <a href="#"><i id="title"></i> Little Sun</a>
         <a href="dashboard.php">Dashboard</a>
-        <a href="location.php">Hub Locations</a>
-        <a href="manager.php">Hub Managers</a>
         <a href="task_types_manager.php">Task types</a>
         <a>Work schedule</a>
         <a>Reports</a>
-        <a>Time-off requests</a>
+        <a href="time_off_request_manager.php">Time-off requests</a>
         <a href="logout.php">Logout</a>
     </div>
     <?php endif; ?>
@@ -32,7 +30,7 @@
     <div class="sidebar">
         <a href="#"><i id="title"></i> Little Sun</a>
         <a href="dashboard.php">Dashboard</a>
-        <a>Time-off requests</a>
+        <a href="time_off_request_employee.php">Time-off requests</a>
         <a>Sick leave</a>
         <a>Clock in/out</a>
         <a>Work schedule</a>
---
+++ time_off_request_employee.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+include 'logged_in.php';
+
+include 'permission_employee.php';
+
+$user = $_SESSION['user'];
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="styles/dashboard.css">
+</head>
+<body>
+<!-- navigation -->    
+<?php include 'navigation.php'; ?>
+
+<!-- dashboard -->    
+
+<div class="main">
+    <h1><span class="title"> Time off requests</span></h1>
+    <div onclick="location.href='request_time_off.php';" class="info-square">Request time off</div>
+</div>
+</body>
+</html>
