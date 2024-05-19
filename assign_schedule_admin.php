cff59273c3aed3b822dcce04daa15e6693a72576
Charlotte <r0952503@student.thomasmore.be>
2024-05-19 20:37:30 +0200

    bug seconden calendar weg

---
+++ assign_schedule_admin.php
@@ -0,0 +1,53 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_start();
+
+require_once __DIR__ . "/classes/User.php";
+require_once __DIR__ . "/classes/Location.php";
+
+include 'logged_in.php';
+
+include 'permission_admin.php';
+
+$user = new User();
+$employees = $user->getEmployeeUsers(); // Get the employee users
+
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Profiles</title>
+    <link rel="stylesheet" href="styles/edit_manager.css">
+    <script src="script/edit_manager.js" defer></script>
+ 
+</head>
+<body>
+
+    <!-- Search bar -->
+    <input type="text" id="searchBar" placeholder="Search for employees..." onkeyup="searchUsers()">
+
+    <!-- User container -->
+
+    <div id="userContainer">
+        <?php foreach ($employees as $employee): ?>
+        <div class="user-box">
+            <img src="../LittleSun-Project/images/profile.jpg" alt="User Profile" class="profile-picture">
+            <div class="user-info">
+    <h2><?= htmlspecialchars($employee['first_name']) ?></h2>
+    <a href="assign_employee.php?user_id=<?= htmlspecialchars($employee['user_id']) ?>" class="delete-button" type="button">Assign work</a>
+</div>
+
+        
+    </div>
+    <?php endforeach; ?>
+    </div>
+
+    <a href="dashboard.php" class="go-back-button" type="button">Go Back</a>
+
+</body>
+
+
+</html>
--- classes/TaskType.php
+++ classes/TaskType.php
@@ -45,6 +45,14 @@ class TaskType {
         $stmt = $conn->query("SELECT task_type_id, task_type_name FROM task_types");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+     // Get assigned task types for a user
+     public function getAssignedTaskTypes($userId) {
+        $conn = $this->db->getConnection();
+        $stmt = $conn->prepare("SELECT task_types.* FROM task_types INNER JOIN user_task_types ON task_types.task_type_id = user_task_types.task_type_id WHERE user_task_types.user_id = ?");
+        $stmt->execute([$userId]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
     
 }
 
