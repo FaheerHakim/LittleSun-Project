d11b3e93498763b60081430c6fb357c8fc59dba7
Charlotte <r0952503@student.thomasmore.be>
2024-05-21 11:50:46 +0200

    alle go-backs een black color instead of link

--- add_location.php
+++ add_location.php
@@ -56,7 +56,7 @@ $existingLocations = $locationHandler->getExistingLocations();
 <head>
     <meta charset="UTF-8">
     <title>Hub locations</title>
-    <link rel="stylesheet" href="styles/location.css">
+    <link rel="stylesheet" href="styles/add_location.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="script/location.js" defer></script>
 </head>
