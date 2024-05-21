5f189b7a5d288e9c51028e29e570ea917071ea3f
Charlotte <r0952503@student.thomasmore.be>
2024-05-21 10:33:57 +0200

    assign tasks to users

--- assign_task.php
+++ assign_task.php
@@ -32,7 +32,7 @@ $taskTypes = $taskTypeHandler->getTaskTypes();
 <head>
     <meta charset="UTF-8">
     <title>Assign Task Types</title>
-    <link rel="stylesheet" href="styles/assign_task_type.css">
+    <link rel="stylesheet" href="styles/assign_task_types.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="script/assign_task_type.js" defer></script>
 </head>
@@ -47,7 +47,6 @@ $taskTypes = $taskTypeHandler->getTaskTypes();
             <?php foreach ($employeeUsers as $user): ?>
                 <div class="user-box">
                     <p><?php echo $user['first_name'] . ' ' . $user['last_name']; ?></p>
-                    <label for="task_type">Assigned task types:</label>
                     <ul>
                         <?php $assignedTaskTypes = $userHandler->getAssignedTaskTypes($user['user_id']); ?>
                         <?php foreach ($assignedTaskTypes as $assignedTaskType): ?>
@@ -63,7 +62,7 @@ $taskTypes = $taskTypeHandler->getTaskTypes();
                             </li>
                         <?php endforeach; ?>
                     </ul>
-                    <form action="assign_task.php" method="post" onsubmit="return confirmAssignment()">
+                    <form action="assign_task.php" method="post">
                         <label for="task_type">Add Task Type:</label>
                         <select name="task_type_id" id="task_type">
                             <?php 
@@ -72,8 +71,10 @@ $taskTypes = $taskTypeHandler->getTaskTypes();
                                 if (!in_array($taskType['task_type_id'], $assignedTaskTypeIds)): ?>
                                     <option value="<?php echo $taskType['task_type_id']; ?>"><?php echo $taskType['task_type_name']; ?></option>
                                 <?php endif; 
+                                
                             endforeach; 
                             ?>
+                           
                         </select>
                         <input type="hidden" name="user_id" value="<?php echo $user['user_id']; ?>">
                         <button class="assign-button" type="submit">Assign Task Type</button>
@@ -83,113 +84,5 @@ $taskTypes = $taskTypeHandler->getTaskTypes();
         </div>
     </div>
 </body>
-<style>
-    body {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        background-color: black;
-        color: white;
-    }
 
-    h1 {
-        text-align: center;
-    }
-
-    .assign-task-container {
-        margin-top: 20px;
-        min-height: 85vh; 
-        padding: 20px;
-        background-color: white;
-        color: black;
-    }
-
-    .go-back-button {
-        display: inline-block;
-        padding: 10px 20px;
-        background-color: #FFDD00;
-        color: black;
-        text-decoration: none;
-        border: none;
-        border-radius: 5px;
-        margin-bottom: 20px;
-        top: 20px;
-        left: 15px;
-        padding: 10px;
-        position: absolute;
-    }
-
-    .sub-container {
-        display: flex;
-        flex-wrap: wrap;
-        gap: 20px;
-    }
-
-    .user-box {
-        width: calc(50% - 20px);
-        background-color: #f9f9f9;
-        border: 1px solid #ddd;
-        padding: 20px;
-        border-radius: 8px;
-        box-sizing: border-box;
-    }
-
-    .user-box p {
-        margin: 0 0 10px;
-        font-weight: bold;
-    }
-
-    ul {
-        list-style-type: none;
-        padding: 0;
-        margin: 0;
-    }
-
-    .task-item {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
-
-    .delete-form {
-        margin: 0;
-    }
-
-    .delete-button {
-        background-color: transparent;
-        border: none;
-        cursor: pointer;
-        color: red;
-    }
-
-    .delete-button i {
-        pointer-events: none;
-    }
-
-    .delete-button:hover {
-        color: darkred;
-    }
-
-    .assign-button {
-        background-color: #007bff;
-        color: white;
-        border: none;
-        padding: 5px 10px;
-        border-radius: 5px;
-        cursor: pointer;
-        margin-top: 7px;
-    }
-
-    .assign-button:hover {
-        background-color: #0056b3;
-    }
-
-    #searchBar {
-        width: 30%;
-        padding: 20px;
-        margin-bottom: 20px;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        box-sizing: border-box;
-    }
-</style>
 </html>
